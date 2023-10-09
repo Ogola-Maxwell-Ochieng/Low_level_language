0cd0a3f379f238004e2cc9974565c999c3af7463
Ogola-Maxwell-Ochieng <maxwelogola5@gmail.com>
2023-10-09 23:26:15 +0530

    Code Functionality and Time Complexity

---
+++ 0001-Introduction/02-puts.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+/*
+ * main - Entry point
+ *
+ * Description: 
+ *
+ * 1.The first line of the program #include <stdio.h> is a preprocessor command,
+ * which tells a C compiler to include stdio.h file before going to actual compilation.
+ *
+ * 2.The next line int main() is the main function where the program execution 
+ * begins.
+ *
+ * 3.The next line puts("\"Programming is cool when you are determined");
+ * and causes the message Programming is cool when you are determined
+ * to be displayed on the screen.
+ *
+ * 4.The next line return 0; terminates main( ) function and causes it to
+ * return the value 0 to the calling process.
+ *
+ * Return: Always  0 (success)
+ */
+int main()
+{
+    // Calling the puts function:
+	puts("\"Programming is cool when you are determined");
+
+    // return value:
+	return (0);
+}
