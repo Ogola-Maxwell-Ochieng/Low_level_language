e41b4811842947fd15a7934a2da364f4d2a1f216
Ogola-Maxwell-Ochieng <maxwelogola5@gmail.com>
2023-10-09 23:22:25 +0530

    Hello, World!

---
+++ 0001-Introduction/01-printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+/*
+*Main-printing  the first program in C
+*
+*Description- writting a C program that prints
+*Hello, World!
+*
+*Return:0 (program successful)
+*/
+int main()
+{
+  //Printing Hello, World! on one line:
+  printf("Hello, World!\n");
+
+  //Return value:
+  return 0;
+}
